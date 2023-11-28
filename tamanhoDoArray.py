3ec9c0df37d56fdb8742d1a8e763eb0877c65957
Andre Viana <91017517+AndreFviana@users.noreply.github.com>
2023-11-28 15:22:39 -0300

    melhorias na execução do código

--- tamanhoDoArray.py
+++ tamanhoDoArray.py
@@ -1,4 +1,7 @@
 #Recebi um array com o nome de vários artistas. Para saber quantas pessoas tem nessa lista eu usei a função len()
-lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+lista_musicos = [ 'Djavan','Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
 totalDeArtistas=len(lista_musicos)
-print(totalDeArtistas)
+#abaixo, foi utilizado a função FORMAT, onde vc coloca um 'f' antes da string e a variavel dentro das chaves.
+print(f'O total de artistas é: {totalDeArtistas}!')
+#Para imprimir um item do array, veja o codigo abaixo
+print(lista_musicos[14])
