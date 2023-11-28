e132a722b21720c97785b1fafec1285a0f4cf13c
Andre Viana <91017517+AndreFviana@users.noreply.github.com>
2023-11-28 10:25:19 -0300

    criando 2 arrays de strings e inteiros

---
+++ criandoArray.py
@@ -0,0 +1,4 @@
+#Criação de 2 arrays. Cada um com 5 elementos, sendo um array só de string's, e o outro só do tipo 'int'.
+listaDeMusicas=['war','toxicity','jet pilot','prison song','deer dance']
+anoDeNascimento=[1992,1995,1996,1997,1998]
+print(listaDeMusicas,anoDeNascimento)
